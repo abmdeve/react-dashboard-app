de9b2f69148f03a92d13c2eaadacc22586756312
abmdeve <tundederome@gmail.com>
2025-05-09 12:03:18 +0200

    Contacts is implemented

--- src/utils/types/types.ts
+++ src/utils/types/types.ts
@@ -8,3 +8,15 @@ export interface TeamMember {
   phone: string;
   access: Access;
 }
+
+export interface Contact_Interface {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  phone: string;
+  address: string;
+  city: string;
+  zipCode: string;
+  registrarId: number;
+}

5a175a2373d2707f8e75de27ca2978d8bd0310a5
abmdeve <tundederome@gmail.com>
2025-05-14 03:20:15 +0200

    Geography Chart screen is implemented

--- src/screens/geography/geography.tsx
+++ src/screens/geography/geography.tsx
@@ -1,7 +1,29 @@
+import { Box, useTheme } from "@mui/material";
 import React from "react";
+import HeaderComponent from "../../components/Header/HeaderComponent";
+import GeographyChart from "../../components/GeographyChart/GeographyChart";
+import { tokens } from "../../context/theme";
 
 const Geography = () => {
-  return <div>Geography</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m={"20px"}>
+      <HeaderComponent
+        title="Geography Chart"
+        subtitle="Simple Geography Chart"
+      />
+
+      <Box
+        height={"75vh"}
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius={"4px"}
+      >
+        <GeographyChart isDashboard={false} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Geography;

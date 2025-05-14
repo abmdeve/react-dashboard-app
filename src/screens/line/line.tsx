f76b7e898cb7001769fdc83c3782196ea1c51ae3
abmdeve <tundederome@gmail.com>
2025-05-14 02:41:37 +0200

    Line Chart screen is implemented

--- src/screens/line/line.tsx
+++ src/screens/line/line.tsx
@@ -1,7 +1,18 @@
+import { Box } from "@mui/material";
 import React from "react";
+import HeaderComponent from "../../components/Header/HeaderComponent";
+import LineChart from "../../components/LineChart/LineChart";
 
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <Box m={"20px"}>
+      <HeaderComponent title="Line Chart" subtitle="Simple Line Chart" />
+
+      <Box height={"75vh"}>
+        <LineChart isDashboard={false} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Line;

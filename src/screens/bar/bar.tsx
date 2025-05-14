f39ecc2f14e7dab37bc9252813715ef0b93084ee
abmdeve <tundederome@gmail.com>
2025-05-14 02:06:05 +0200

    Pie screen is implemented

--- src/screens/bar/bar.tsx
+++ src/screens/bar/bar.tsx
@@ -1,7 +1,20 @@
+import { Box } from "@mui/material";
 import React from "react";
+import HeaderComponent from "../../components/Header/HeaderComponent";
+import BarChart from "../../components/BarChart/BarChart";
 
 const Bar = () => {
-  return <div>Bar</div>;
+  return (
+    <Box m={"20px"}>
+      <Box>
+        <HeaderComponent title="Bar Chart" subtitle="Simple Bar Chart" />
+      </Box>
+
+      <Box height={"75vh"}>
+        <BarChart isDashboard={false} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Bar;
--- src/screens/pie/pie.tsx
+++ src/screens/pie/pie.tsx
@@ -1,7 +1,18 @@
+import { Box } from "@mui/material";
 import React from "react";
+import HeaderComponent from "../../components/Header/HeaderComponent";
+import PieChart from "../../components/PieChart/PieChart";
 
 const Pie = () => {
-  return <div>Pie</div>;
+  return (
+    <Box m={"20px"}>
+      <HeaderComponent title="Pie Chart" subtitle="Simple Pie Chart" />
+
+      <Box height={"75vh"}>
+        <PieChart isDashboard={false} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Pie;
